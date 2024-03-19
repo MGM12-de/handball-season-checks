237a47b318e5fa2132792125a314c4e7e6a2abf1
Miguel <miggi92@users.noreply.github.com>
2024-03-19 15:58:12 +0000

    feat: :sparkles: init startpage with content

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-
+    <UPage>
+        <UPageHeader headline="Handball" title="Season checks" description="A website to check the current season" />
+        <UPageBody>
+            <ULandingHero title="Turn Your Vision into Reality"
+                description="Watch your dreams materialize before your eyes with us." />
+        </UPageBody>
+    </UPage>
 </template>
 <script>
 </script>
