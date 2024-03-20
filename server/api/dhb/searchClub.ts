33b83a31c5ad81e9a15f2067fd735bd318e48362
Miguel <miggi92@users.noreply.github.com>
2024-03-20 15:07:53 +0000

    feat: :sparkles: added api call to find club

---
+++ server/api/dhb/searchClub.ts
@@ -0,0 +1,13 @@
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+
+  if (!query.clubName) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Expected a clubname but got none',
+    })
+  }
+  const clubs = await $fetch(`https://www.handball.net/a/sportdata/1/clubs/search?query=${query.clubName}`)
+
+  return clubs.data
+})
