a8ca050a86dcbf28c6935f838c96d6d56ee75fcd
Miguel <miggi92@users.noreply.github.com>
2024-04-09 22:47:23 +0000

    feat: :sparkles: added average goals

--- components/team/prognose.vue
+++ components/team/prognose.vue
@@ -2,6 +2,14 @@
   <div>
     <UContainer>
       <UForm :state="state" class="space-y-4">
+        <UFormGroup label="Durchschnittlich geworfene Tore">
+          <UInput v-model="state.averageGoalsShot" disabled />
+        </UFormGroup>
+
+        <UFormGroup label="Durchschnittlich bekommene Tore">
+          <UInput v-model="state.averageGoalsGot" disabled />
+        </UFormGroup>
+        <br />
         <UTabs :items="items" class="w-full">
           <template #default="{ item, index, selected }">
             <div class="flex items-center gap-2 relative truncate">
@@ -22,7 +30,19 @@
             </UFormGroup>
 
             <UFormGroup label="Heimquote">
-              <UInput v-model="state.homeQuota" disabled />
+              <UInput v-model="state.homeQuota" disabled>
+                <template #trailing>
+                  <span class="text-gray-500 dark:text-gray-400 text-xs">%</span>
+                </template>
+              </UInput>
+            </UFormGroup>
+
+            <UFormGroup label="Durchschnittlich Daheim geworfene Tore">
+              <UInput v-model="state.homeAverageGoalsShot" disabled />
+            </UFormGroup>
+
+            <UFormGroup label="Durchschnittlich Daheim bekommene Tore">
+              <UInput v-model="state.homeAverageGoalsGot" disabled />
             </UFormGroup>
 
           </template>
@@ -36,7 +56,19 @@
             </UFormGroup>
 
             <UFormGroup label="Auswärtsquote">
-              <UInput v-model="state.awayQuota" disabled />
+              <UInput v-model="state.awayQuota" disabled>
+                <template #trailing>
+                  <span class="text-gray-500 dark:text-gray-400 text-xs">%</span>
+                </template>
+              </UInput>
+            </UFormGroup>
+
+            <UFormGroup label="Durchschnittlich Auswärts geworfene Tore">
+              <UInput v-model="state.awayAverageGoalsShot" disabled />
+            </UFormGroup>
+
+            <UFormGroup label="Durchschnittlich Auswärts bekommene Tore">
+              <UInput v-model="state.awayAverageGoalsGot" disabled />
             </UFormGroup>
           </template>
         </UTabs>
@@ -57,7 +89,13 @@ const state = reactive({
   highestHomeLoose: undefined,
   highestAwayLoose: undefined,
   awayQuota: 0,
-  homeQuota: 0
+  homeQuota: 0,
+  averageGoalsShot: 0,
+  averageGoalsGot: 0,
+  homeAverageGoalsShot: 0,
+  homeAverageGoalsGot: 0,
+  awayAverageGoalsShot: 0,
+  awayAverageGoalsGot: 0
 })
 
 const items = [{
@@ -90,68 +128,83 @@ var stats = ref({
   highestAwayWin: {},
   homeWins: 0,
   homeGames: 0,
+  homeGoalsShot: 0,
+  homeGoalsGot: 0,
   awayWins: 0,
-  awayGames: 0
+  awayGames: 0,
+  awayGoalsShot: 0,
+  awayGoalsGot: 0,
 })
 
 const { teamId, games, gamesPending } = props;
 
 games.forEach(element => {
-  if (element.homeTeam.id === teamId && element.goalDifference > 0) {
-    stats.value.homeGames++
-    stats.value.homeWins++
-    if (stats.highestHomeWin) {
-      if (stats.highestHomeWin.goalDifference < element.goalDifference)
-        stats.highestHomeWin = element
-    } else {
-      stats.highestHomeWin = element
-    }
-  }
-  if (stats.highestHomeWin)
-    state.highestHomeWin = `${stats.highestHomeWin.awayTeam.name} (${stats.highestHomeWin.result})`
+  if (element.homeTeam.id === teamId) {
+    stats.value.homeGames++;
+    stats.value.homeGoalsShot += element.homeGoals;
+    stats.value.homeGoalsGot += element.awayGoals;
 
-  if (element.homeTeam.id === teamId && element.goalDifference < 0) {
-    stats.value.homeGames++
-    if (stats.highestHomeLoose) {
-      if (stats.highestHomeLoose.goalDifference > element.goalDifference)
-        stats.highestHomeLoose = element
-    } else {
-      stats.highestHomeLoose = element
-    }
-  }
-  if (stats.highestHomeLoose)
-    state.highestHomeLoose = `${stats.highestHomeLoose.awayTeam.name} (${stats.highestHomeLoose.result})`
+    if (element.goalDifference > 0) {
+      stats.value.homeWins++;
 
-  if (element.awayTeam.id === teamId && element.goalDifference < 0) {
-    stats.value.awayGames++
-    stats.value.awayWins++
-    if (stats.highestAwayWin) {
-      if (stats.highestAwayWin.goalDifference > element.goalDifference)
-        stats.highestAwayWin = element
-    } else {
-      stats.highestAwayWin = element
+      if (!stats.highestHomeWin || stats.highestHomeWin.goalDifference < element.goalDifference) {
+        stats.highestHomeWin = element;
+      }
+    } else if (element.goalDifference < 0) {
+      if (!stats.highestHomeLoose || stats.highestHomeLoose.goalDifference > element.goalDifference) {
+        stats.highestHomeLoose = element;
+      }
     }
   }
 
-  if (stats.highestAwayWin)
-    state.highestAwayWin = `${stats.highestAwayWin.homeTeam.name} (${stats.highestAwayWin.result})`
+  if (stats.highestHomeWin) {
+    state.highestHomeWin = `${stats.highestHomeWin.awayTeam.name} (${stats.highestHomeWin.result})`;
+  }
 
-  if (element.awayTeam.id === teamId && element.goalDifference > 0) {
-    stats.value.awayGames++
-    if (stats.highestAwayLoose) {
-      if (stats.highestAwayLoose.goalDifference < element.goalDifference)
-        stats.highestAwayLoose = element
-    } else {
-      stats.highestAwayLoose = element
+  if (stats.highestHomeLoose) {
+    state.highestHomeLoose = `${stats.highestHomeLoose.awayTeam.name} (${stats.highestHomeLoose.result})`;
+  }
+
+  if (element.awayTeam.id === teamId) {
+    stats.value.awayGames++;
+    stats.value.awayGoalsShot += element.awayGoals;
+    stats.value.awayGoalsGot += element.homeGoals;
+
+    if (element.goalDifference < 0) {
+      stats.value.awayWins++;
+
+      if (!stats.highestAwayWin || stats.highestAwayWin.goalDifference > element.goalDifference) {
+        stats.highestAwayWin = element;
+      }
+    } else if (element.goalDifference > 0) {
+      if (!stats.highestAwayLoose || stats.highestAwayLoose.goalDifference < element.goalDifference) {
+        stats.highestAwayLoose = element;
+      }
     }
   }
-  if (stats.highestAwayLoose)
-    state.highestAwayLoose = `${stats.highestAwayLoose.homeTeam.name} (${stats.highestAwayLoose.result})`
+
+  if (stats.highestAwayWin) {
+    state.highestAwayWin = `${stats.highestAwayWin.homeTeam.name} (${stats.highestAwayWin.result})`;
+  }
+
+  if (stats.highestAwayLoose) {
+    state.highestAwayLoose = `${stats.highestAwayLoose.homeTeam.name} (${stats.highestAwayLoose.result})`;
+  }
 
   // calc quotas
-  state.homeQuota = Math.round((stats.value.homeWins / stats.value.homeGames) * 100).toFixed(2)
-  state.awayQuota = Math.round((stats.value.awayWins / stats.value.awayGames) * 100).toFixed(2)
+  state.homeQuota = ((stats.value.homeWins / stats.value.homeGames) * 100).toFixed(2)
+  state.awayQuota = ((stats.value.awayWins / stats.value.awayGames) * 100).toFixed(2)
+  // calc average goals
+  state.homeAverageGoalsShot = Math.round(stats.value.homeGoalsShot / stats.value.homeGames).toFixed(2)
+  state.homeAverageGoalsGot = Math.round(stats.value.homeGoalsGot / stats.value.homeGames).toFixed(2)
+  state.awayAverageGoalsShot = Math.round(stats.value.awayGoalsShot / stats.value.awayGames).toFixed(2)
+  state.awayAverageGoalsGot = Math.round(stats.value.awayGoalsGot / stats.value.awayGames).toFixed(2)
+
+  state.averageGoalsShot = Math.round((stats.value.homeGoalsShot + stats.value.awayGoalsShot) / (stats.value.homeGames + stats.value.awayGames)).toFixed(2)
+  state.averageGoalsGot = Math.round((stats.value.homeGoalsGot + stats.value.awayGoalsGot) / (stats.value.homeGames + stats.value.awayGames)).toFixed(2)
 });
+
+
 
 </script>
 
