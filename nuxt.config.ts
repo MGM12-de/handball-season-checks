580103cd471b73f7faaf7cb2ae9224719126c197
Miguel <miggi92@users.noreply.github.com>
2024-04-30 10:30:09 +0000

    feat: :sparkles: added scalar

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/content', '@nuxt/ui', "@nuxt/image"],
+  modules: ["@scalar/nuxt", '@nuxt/content', '@nuxt/ui', "@nuxt/image"],
   app: {
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     }
+  },
+  nitro: {
+    experimental: {
+      openAPI: true,
+    },
   },
   ui: {
     icons: ['mdi', 'simple-icons']
