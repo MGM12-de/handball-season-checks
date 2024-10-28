1d46925916eb6a9944b38fd5fe8017d880cfd0ad
miguel.gebhardt <miguel.gebhardt@googlemail.com>
2024-10-28 19:49:02 +0000

    feat: :sparkles: added api doc

--- app.config.ts
+++ app.config.ts
@@ -1,6 +1,7 @@
 export default defineAppConfig({
     ui: {
         primary: 'amber',
+        secondary: 'sky',
         gray: 'cool'
     }
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/content', '@nuxt/ui', "@nuxt/image"],
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    "@nuxt/image",
+    '@scalar/nuxt',
+  ],
   app: {
     head: {
       charset: 'utf-8',
@@ -14,8 +19,5 @@ export default defineNuxtConfig({
     experimental: {
       openAPI: true,
     },
-  },
-  ui: {
-    icons: ['mdi', 'simple-icons']
   },
 })
