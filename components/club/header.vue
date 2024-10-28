5fcb14ffc3dc8376d194ac33b02375a310fc4692
miguel.gebhardt <miguel.gebhardt@googlemail.com>
2024-10-28 20:53:06 +0000

    feat: :sparkles: added memberclubs

--- components/club/header.vue
+++ components/club/header.vue
@@ -6,13 +6,19 @@
         <UAvatar :src="club.organization.logo" :alt="club.organization.acronym" />
         <UAvatar :src="club.logo" :alt="club.acronym" />
       </UAvatarGroup>
+      <div v-if="club.hasMemberClubs">
+        <h2>Stammvereine</h2>
+        <div v-for="member in memberClubs">
+          <span>{{ member.name }}</span>
+        </div>
+      </div>
       <ClubInfo :club="club" />
     </ULandingCard>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+let memberClubs = reactive([])
 const props = defineProps({
   club: {
     type: Object,
@@ -20,6 +26,19 @@ const props = defineProps({
   }
 })
 const { club } = props;
+
+if(club.hasMemberClubs){
+  useAsyncData(
+  `club/${club.id}/memberClubs`,
+  () => $fetch("/api/dhb/club/memberClubs", {
+    query: { id: club.id }
+  })
+).then(
+  (response) => {
+    memberClubs = response.data
+  }
+)
+}
 </script>
 
 <style></style>
---
+++ server/api/dhb/club/memberClubs.ts
@@ -0,0 +1,18 @@
+/**
+ * Get members clubs
+ */
+export default defineEventHandler(async (event) => {
+
+  //https://www.handball.net/a/sportdata/1/clubs/handball4all.wuerttemberg.1187/member-clubs
+  const query = getQuery(event)
+
+  if (!query.id) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'No id received',
+    })
+  }
+  const clubInfo = await $fetch(`https://www.handball.net/a/sportdata/1/clubs/${query.id}/member-clubs`)
+
+  return clubInfo.data
+})
