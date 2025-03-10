2b9e9dddc44c2987388b7c9d722557d6b8fc6eee
Miguel <miggi92@users.noreply.github.com>
2025-03-10 17:56:58 +0000

    feat: :sparkles: added tournament

---
+++ app/pages/tournament/[...id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script lang="ts" setup>
+const route = useRoute()
+
+useSeoMeta({
+  title: `Tournament`,
+  description: '',
+})
+</script>
+
+<style></style>
