7471727df65fbd1f4d964a346a658e6117a5a431
Miguel <miggi92@users.noreply.github.com>
2025-03-11 00:39:55 +0000

    feat: :sparkles: added lineup

--- app/pages/tournament/[...id].vue
+++ app/pages/tournament/[...id].vue
@@ -2,7 +2,17 @@
   <UPage>
     <UPageHeader :headline="tournament.acronym" :title="tournament.name" description="" />
     <UPageBody>
-      <UTable :rows="tournamentTable" :columns="columns" />
+      <UTabs :items="items" class="w-full">
+        <template #standing>
+          <UTable :rows="tournamentTable" :columns="columns" />
+        </template>
+        <template #stats>
+          <div>Stats</div>
+        </template>
+        <template #lineup>
+          <UTable :rows="tournamentLineup" :columns="lineupCols" :sort="sortLineup" />
+        </template>
+      </UTabs>
     </UPageBody>
   </UPage>
 </template>
@@ -14,6 +24,20 @@ useSeoMeta({
   title: `Tournament`,
   description: '',
 })
+
+const items = [{
+  slot: 'standing',
+  label: 'Standing',
+  icon: 'i-mdi-trophy',
+}, {
+  slot: 'stats',
+  label: 'Statistics',
+  icon: 'i-mdi-chart-bar',
+}, {
+  slot: 'lineup',
+  label: 'Lineup',
+  icon: 'i-mdi-account-group',
+}]
 
 const { data: tournament, status: tournamentStatus } = await useAsyncData(
   `tournament/${route.params.id}`,
@@ -28,6 +52,25 @@ const columns = [{ key: 'rank', label: 'Rank' }, { key: 'team.name', label: 'Tea
 const { data: tournamentTable, status: tournamentTableStatus } = await useAsyncData(
   `tournament/${route.params.id}/table`,
   () => $fetch('/api/dhb/tournament/table', {
+    query: { id: route.params.id },
+  }),
+)
+
+const lineupCols = [{ key: 'firstname', label: 'Firstname', sortable: true }, { key: 'lastname', label: 'Lastname', sortable: true },
+{ key: 'team.name', label: 'Team', sortable: true },
+{ key: 'gamesPlayed', label: 'Games played', sortable: true }, { key: 'goals', label: 'Goals', sortable: true },
+// { key: 'penaltyGoals', label: 'Penalty goals', sortable: true }, { key: 'penaltyMissed', label: 'Penalty missed', sortable: true },
+{ key: 'yellowCards', label: 'Yellow Cards', sortable: true }, { key: 'penalties', label: 'Penalties', sortable: true },
+{ key: 'redCards', label: 'Red Cards', sortable: true }, { key: 'blueCards', label: 'Blue Cards', sortable: true }]
+
+const sortLineup = ref<{ column: string; direction: 'desc' | 'asc' }>({
+  column: 'goals',
+  direction: 'desc'
+})
+
+const { data: tournamentLineup, status: tournamentLineupStatus } = await useAsyncData(
+  `tournament/${route.params.id}/lineup`,
+  () => $fetch('/api/dhb/tournament/lineup', {
     query: { id: route.params.id },
   }),
 )
