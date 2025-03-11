64ac65f4ad443ab80019f41468c84448e15bf3de
Miguel <miggi92@users.noreply.github.com>
2025-03-11 00:58:29 +0000

    feat: :sparkles: added tournament search

---
+++ app/components/tournament/search.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+const state = reactive({
+  tournament: undefined,
+})
+const clubs = ref([])
+const loading = ref(false)
+
+const columns = [{
+  key: 'logo',
+  label: 'Logo',
+}, {
+  key: 'name',
+  label: 'Name',
+}, {
+  key: 'acronym',
+  label: 'Acronym',
+}, {
+  key: 'organization',
+  label: 'Organization',
+}]
+
+async function onSearch() {
+  loading.value = true
+  const { data } = useAsyncData(`${state.tournament}`, () => $fetch('/api/dhb/tournament/search', {
+    query: { tournamentName: state.tournament },
+  }))
+  clubs.value = data.value
+  loading.value = false
+}
+
+function onRowSelected(row) {
+  navigateTo(`/club/details/${row.id}`)
+}
+</script>
+
+<template>
+  <div>
+    <UForm class="space-y-4" @submit="onSearch" :state="state">
+      <UFormGroup label="Tournament">
+        <UInput v-model="state.tournament" />
+      </UFormGroup>
+      <UButton type="submit">
+        Search
+      </UButton>
+    </UForm>
+
+    <UTable :rows="clubs" :columns="columns" :loading="loading" @select="onRowSelected">
+      <template #logo-data="{ row }">
+        <UAvatar :src="row.logo" :alt="row.acronym" />
+      </template>
+
+      <template #organization-data="{ row }">
+        <UAvatar :src="row.organization.logo" :alt="row.organization.name" />
+        <span>{{ row.organization.name }}</span>
+      </template>
+    </UTable>
+  </div>
+</template>
+
+<style></style>
---
+++ server/api/dhb/tournament/search.ts
@@ -0,0 +1,72 @@
+import { getClubsUrl, normalizeDHBUrl } from '../../../../server/utils/dhbUtils'
+
+defineRouteMeta({
+  openAPI: {
+    description: 'Search for a club',
+    summary: 'Search for a club',
+    tags: ['DHB'],
+    parameters: [
+      {
+        in: 'query',
+        name: 'tournamentName',
+        required: true,
+        example: 'BOL_HF',
+      }],
+    responses: {
+      400: {
+        description: "Bad Request",
+        summary: "Expected a clubname but got none",
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/components/schemas/Error'
+            }
+          }
+        }
+      },
+      200: {
+        description: "Found a club",
+        summary: "Found a club",
+        content: {
+          'application/json': {
+            schema: {
+              type: 'array',
+              items: {
+                $ref: '#/components/schemas/Club'
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+})
+
+/**
+ * Search for club
+ * @param {import('node:http').IncomingMessage} event
+ * @returns {Promise<Club[]>}
+ * @throws {Error}
+ */
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+
+  if (!query.tournamentName) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Expected a tournamentName but got none',
+    })
+  }
+  const tournaments = await $fetch(`${getTournamentsUrl()}/search?query=${query.tournamentName}`)
+
+  tournaments.data.forEach((tournament) => {
+    if (tournament.logo) {
+      tournament.logo = normalizeDHBUrl(tournament.logo)
+    }
+
+    if (tournament.organization.logo) {
+      tournament.organization.logo = normalizeDHBUrl(tournament.organization.logo)
+    }
+  })
+  return tournaments.data
+})
