83311ab6823742507e69fd21812162bc2f085914
Miguel <miggi92@users.noreply.github.com>
2025-03-26 23:05:46 +0000

    refactor: :recycle: changed to nuxt ui v3

--- app/pages/tournament/[...id].vue
+++ app/pages/tournament/[...id].vue
@@ -1,23 +1,11 @@
-<template>
-  <UPage>
-    <UPageHeader :headline="tournament.acronym" :title="tournament.name" description="" />
-    <UPageBody>
-      <UTabs :items="items" class="w-full">
-        <template #standing>
-          <UTable :rows="tournamentTable" :columns="columns" />
-        </template>
-        <template #stats>
-          <div>Stats</div>
-        </template>
-        <template #lineup>
-          <UTable :rows="tournamentLineup" :columns="lineupCols" :sort="sortLineup" />
-        </template>
-      </UTabs>
-    </UPageBody>
-  </UPage>
-</template>
-
 <script lang="ts" setup>
+import type { TableColumn, TableRow } from '@nuxt/ui'
+import type { Column } from '@tanstack/vue-table'
+import { h, resolveComponent } from 'vue'
+
+const UButton = resolveComponent('UButton')
+const UDropdownMenu = resolveComponent('UDropdownMenu')
+
 const route = useRoute()
 
 useSeoMeta({
@@ -27,27 +15,26 @@ useSeoMeta({
 
 const items = [{
   slot: 'standing',
-  label: 'Standing',
+  header: 'Standing',
   icon: 'i-mdi-trophy',
 }, {
   slot: 'stats',
-  label: 'Statistics',
+  header: 'Statistics',
   icon: 'i-mdi-chart-bar',
 }, {
   slot: 'lineup',
-  label: 'Lineup',
+  header: 'Lineup',
   icon: 'i-mdi-account-group',
 }]
 
-const { data: tournament, status: tournamentStatus } = await useAsyncData(
+const { data: tournament } = await useAsyncData(
   `tournament/${route.params.id}`,
   () => $fetch('/api/dhb/tournament', {
     query: { id: route.params.id },
   }),
 )
 
-const columns = [{ key: 'rank', label: 'Rank' }, { key: 'team.name', label: 'Team' }, { key: 'points', label: 'Points' }, { key: 'games', label: 'Games' }, { key: 'wins', label: 'Wins' }, { key: 'draws', label: 'Draws' }, { key: 'losses', label: 'Losses' }, { key: 'goals', label: 'Goals' }, { key: 'goalsAgainst', label: 'Goals Against' }, { key: 'goalDifference', label: 'Goal Difference' }]
-
+const columns: TableColumn<any>[] = [{ accessorKey: 'rank', header: 'Rank' }, { accessorKey: 'team.name', header: 'Team' }, { accessorKey: 'points', header: 'Points' }, { accessorKey: 'games', header: 'Games' }, { accessorKey: 'wins', header: 'Wins' }, { accessorKey: 'draws', header: 'Draws' }, { accessorKey: 'losses', header: 'Losses' }, { accessorKey: 'goals', header: 'Goals' }, { accessorKey: 'goalsAgainst', header: 'Goals Against' }, { accessorKey: 'goalDifference', header: 'Goal Difference' }]
 
 const { data: tournamentTable, status: tournamentTableStatus } = await useAsyncData(
   `tournament/${route.params.id}/table`,
@@ -56,17 +43,9 @@ const { data: tournamentTable, status: tournamentTableStatus } = await useAsyncD
   }),
 )
 
-const lineupCols = [{ key: 'firstname', label: 'Firstname', sortable: true }, { key: 'lastname', label: 'Lastname', sortable: true },
-{ key: 'team.name', label: 'Team', sortable: true },
-{ key: 'gamesPlayed', label: 'Games played', sortable: true }, { key: 'goals', label: 'Goals', sortable: true },
-// { key: 'penaltyGoals', label: 'Penalty goals', sortable: true }, { key: 'penaltyMissed', label: 'Penalty missed', sortable: true },
-{ key: 'yellowCards', label: 'Yellow Cards', sortable: true }, { key: 'penalties', label: 'Penalties', sortable: true },
-{ key: 'redCards', label: 'Red Cards', sortable: true }, { key: 'blueCards', label: 'Blue Cards', sortable: true }]
-
-const sortLineup = ref<{ column: string; direction: 'desc' | 'asc' }>({
-  column: 'goals',
-  direction: 'desc'
-})
+const lineupCols: TableColumn<any>[] = [{ accessorKey: 'firstname', header: ({ column }) => getHeader(column, 'Firstname') }, { accessorKey: 'lastname', header: ({ column }) => getHeader(column, 'Lastname') }, { accessorKey: 'team.name', header: ({ column }) => getHeader(column, 'Team') }, { accessorKey: 'gamesPlayed', header: ({ column }) => getHeader(column, 'Games played') }, { accessorKey: 'goals', header: ({ column }) => getHeader(column, 'Goals') },
+// { accessorKey: 'penaltyGoals', header: 'Penalty goals' }, { accessorKey: 'penaltyMissed', header: 'Penalty missed' },
+{ accessorKey: 'yellowCards', header: ({ column }) => getHeader(column, 'Yellow Cards') }, { accessorKey: 'penalties', header: ({ column }) => getHeader(column, 'Penalties') }, { accessorKey: 'redCards', header: ({ column }) => getHeader(column, 'Red Cards') }, { accessorKey: 'blueCards', header: ({ column }) => getHeader(column, 'Blue Cards') }]
 
 const { data: tournamentLineup, status: tournamentLineupStatus } = await useAsyncData(
   `tournament/${route.params.id}/lineup`,
@@ -75,6 +54,89 @@ const { data: tournamentLineup, status: tournamentLineupStatus } = await useAsyn
   }),
 )
 
+const sorting = ref([
+  {
+    id: 'goals',
+    desc: true,
+  },
+])
+
+function getHeader(column: Column<any>, label: string) {
+  const isSorted = column.getIsSorted()
+
+  return h(
+    UDropdownMenu,
+    {
+      'content': {
+        align: 'start',
+      },
+      'aria-label': 'Actions dropdown',
+      'items': [
+        {
+          label: 'Asc',
+          type: 'checkbox',
+          icon: 'i-lucide-arrow-up-narrow-wide',
+          checked: isSorted === 'asc',
+          onSelect: () => {
+            if (isSorted === 'asc') {
+              column.clearSorting()
+            }
+            else {
+              column.toggleSorting(false)
+            }
+          },
+        },
+        {
+          label: 'Desc',
+          icon: 'i-lucide-arrow-down-wide-narrow',
+          type: 'checkbox',
+          checked: isSorted === 'desc',
+          onSelect: () => {
+            if (isSorted === 'desc') {
+              column.clearSorting()
+            }
+            else {
+              column.toggleSorting(true)
+            }
+          },
+        },
+      ],
+    },
+    () =>
+      h(UButton, {
+        'color': 'neutral',
+        'variant': 'ghost',
+        label,
+        'icon': isSorted
+          ? isSorted === 'asc'
+            ? 'i-lucide-arrow-up-narrow-wide'
+            : 'i-lucide-arrow-down-wide-narrow'
+          : 'i-lucide-arrow-up-down',
+        'class': '-mx-2.5 data-[state=open]:bg-(--ui-bg-elevated)',
+        'aria-label': `Sort by ${isSorted === 'asc' ? 'descending' : 'ascending'}`,
+      }),
+  )
+}
 </script>
+
+<template>
+  <UPage>
+    <UPageHeader :headline="tournament.acronym" :title="tournament.name" description="" />
+    <UPageBody>
+      <UTabs :items="items" class="w-full">
+        <template #standing>
+          <UTable :data="tournamentTable" :columns="columns" :loading="tournamentTableStatus === 'pending'" />
+        </template>
+        <template #stats>
+          <div>Stats</div>
+        </template>
+        <template #lineup>
+          <UTable v-model:sorting="sorting" :data="tournamentLineup" :columns="lineupCols"
+            :loading="tournamentLineupStatus === 'pending'" class="flex-1" />
+        </template>
+      </UTabs>
+    </UPageBody>
+  </UPage>
+</template>
 
 <style></style>
