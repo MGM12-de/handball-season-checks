2bf69bf694a4813737b14160ff8ae88a19430c25
Miguel <miggi92@users.noreply.github.com>
2025-03-27 00:46:37 +0000

    chore: :globe_with_meridians: update translations

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,10 @@
 <script lang="ts" setup>
+const { t } = useI18n()
 </script>
 
 <template>
   <UPage>
-    <UPageHeader :headline="$t('handball')" :title="$t('siteTitle')"
+    <UPageHeader :headline="t('handball')" :title="t('siteTitle')"
       description="A website to check the current season" />
     <UPageBody>
       <ClubSearch />
