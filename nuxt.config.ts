85dc69dd888bfccabec2abf15b515bce6acfa882
Miguel <miggi92@users.noreply.github.com>
2025-03-27 01:16:52 +0000

    feat: :sparkles: update translations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,8 +33,9 @@ export default defineNuxtConfig({
     strategy: 'no_prefix',
     defaultLocale: 'en',
     baseUrl: 'https://handball-checks.mgm12.dev',
-    locales: [{ code: 'en', name: 'English', language: 'en-US', file: 'en-US.ts' }, { code: 'de', name: 'Deutsch', language: 'de-DE', file: 'de-DE.ts' }],
+    locales: [{ code: 'en', name: 'English', language: 'en-US', file: 'en-US.json' }, { code: 'de', name: 'Deutsch', language: 'de-DE', file: 'de-DE.json' }],
     lazy: true,
+    langDir: 'locales',
     detectBrowserLanguage: {
       useCookie: true,
       cookieCrossOrigin: true,
