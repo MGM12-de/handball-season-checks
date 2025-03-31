df36d80ec2b0b361174808c25f0e149b9fb98596
Miguel <miguel.gebhardt@bechtle.com>
2025-03-31 23:49:27 +0200

    feat: :sparkles: added game error handling

--- server/api/dhb/team/games.ts
+++ server/api/dhb/team/games.ts
@@ -12,68 +12,77 @@ defineRouteMeta({
         name: 'id',
         required: true,
         example: 'handball4all.wuerttemberg.36',
-        summary: 'Team id'
-      }],
-  }
+        summary: 'Team id',
+      },
+    ],
+  },
 })
 
 export default defineEventHandler(async (event) => {
-  const query = getQuery(event)
+  try {
+    const query = getQuery(event)
 
-  const games: Game[] = []
+    const games: Game[] = []
 
-  if (!query.id) {
+    if (!query.id) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'No id received',
+      })
+    }
+
+    const teamId = query.id as string
+    const teamApi = await $fetch(`${getTeamUrl(teamId)}/schedule`)
+
+    teamApi.data.forEach((element) => {
+      if (element.startsAt) {
+        const date = new Date(element.startsAt)
+        element.startsAt = `${date.toLocaleDateString('de')}, ${date.toLocaleTimeString('de', {
+          hour: '2-digit',
+          minute: '2-digit',
+        })}`
+      }
+      if (element.homeGoals) {
+        element.result = `${element.homeGoals}:${element.awayGoals}`
+      }
+
+      games.push({
+        id: element.id,
+        startsAt: element.startsAt,
+        homeTeam: {
+          id: element.homeTeam.id,
+          teamGroupId: element.homeTeam.teamGroupId,
+          name: element.homeTeam.name,
+          acronym: element.homeTeam.acronym,
+        },
+        awayTeam: {
+          id: element.awayTeam.id,
+          teamGroupId: element.awayTeam.teamGroupId,
+          name: element.awayTeam.name,
+          acronym: element.awayTeam.acronym,
+        },
+        tournament: {
+          id: element.tournament.id,
+          name: element.tournament.name,
+        },
+        result: element.result,
+        homeGoals: element.homeGoals,
+        awayGoals: element.awayGoals,
+        goalDifference: element.homeGoals - element.awayGoals,
+        remarks: element.remarks,
+        field: {
+          id: element.field.id,
+          name: element.field.name,
+        },
+        pdfUrl: element.pdfUrl,
+      })
+    })
+    return games
+  }
+  catch (error) {
     throw createError({
-      statusCode: 400,
-      statusMessage: 'No id received',
+      statusCode: 500,
+      statusMessage: `Error fetching team game data. (${error})`,
     })
   }
-
-  const teamId = query.id as string
-  const teamApi = await $fetch(`${getTeamUrl(teamId)}/schedule`)
-
-  teamApi.data.forEach((element) => {
-    if (element.startsAt) {
-      const date = new Date(element.startsAt)
-      element.startsAt = `${date.toLocaleDateString('de')}, ${date.toLocaleTimeString('de', {
-        hour: '2-digit',
-        minute: '2-digit',
-      })}`
-    }
-    if (element.homeGoals) {
-      element.result = `${element.homeGoals}:${element.awayGoals}`
-    }
-
-    games.push({
-      id: element.id,
-      startsAt: element.startsAt,
-      homeTeam: {
-        id: element.homeTeam.id,
-        teamGroupId: element.homeTeam.teamGroupId,
-        name: element.homeTeam.name,
-        acronym: element.homeTeam.acronym,
-      },
-      awayTeam: {
-        id: element.awayTeam.id,
-        teamGroupId: element.awayTeam.teamGroupId,
-        name: element.awayTeam.name,
-        acronym: element.awayTeam.acronym,
-      },
-      tournament: {
-        id: element.tournament.id,
-        name: element.tournament.name,
-      },
-      result: element.result,
-      homeGoals: element.homeGoals,
-      awayGoals: element.awayGoals,
-      goalDifference: element.homeGoals - element.awayGoals,
-      remarks: element.remarks,
-      field: {
-        id: element.field.id,
-        name: element.field.name,
-      },
-      pdfUrl: element.pdfUrl,
-    })
-  })
-  return games
 })
