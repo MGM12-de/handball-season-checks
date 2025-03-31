8b32f852c2acb921d1e5dfd3b53155606c72f950
Miguel <miguel.gebhardt@bechtle.com>
2025-03-31 17:44:03 +0200

    fix: :bug: fixed sorting of team linup not working

--- app/components/team/lineup.vue
+++ app/components/team/lineup.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import type { TableColumn } from '@nuxt/ui'
+import type { Column } from '@tanstack/vue-table'
 
 const props = defineProps({
   teamId: {
@@ -7,9 +8,13 @@ const props = defineProps({
     required: true,
   },
 })
+
+const UButton = resolveComponent('UButton')
+const UDropdownMenu = resolveComponent('UDropdownMenu')
+
 const { teamId } = props
 
-const columns: TableColumn<any>[] = [{ accessorKey: 'firstname', header: 'Firstname' }, { accessorKey: 'lastname', header: 'Lastname' }, { accessorKey: 'gamesPlayed', header: 'Games played' }, { accessorKey: 'goals', header: 'Goals' }, { accessorKey: 'penaltyGoals', header: 'Penalty goals' }, { accessorKey: 'penaltyMissed', header: 'Penalty missed' }, { accessorKey: 'yellowCards', header: 'Yellow Cards' }, { accessorKey: 'penalties', header: 'Penalties' }, { accessorKey: 'redCards', header: 'Red Cards' }, { accessorKey: 'blueCards', header: 'Blue Cards' }]
+const columns: TableColumn<any>[] = [{ accessorKey: 'firstname', header: ({ column }) => getHeader(column, 'Firstname') }, { accessorKey: 'lastname', header: ({ column }) => getHeader(column, 'Lastname') }, { accessorKey: 'gamesPlayed', header: ({ column }) => getHeader(column, 'Games played') }, { accessorKey: 'goals', header: ({ column }) => getHeader(column, 'Goals') }, { accessorKey: 'yellowCards', header: ({ column }) => getHeader(column, 'Yellow Cards') }, { accessorKey: 'penalties', header: ({ column }) => getHeader(column, 'Penalties') }, { accessorKey: 'redCards', header: ({ column }) => getHeader(column, 'Red Cards') }, { accessorKey: 'blueCards', header: ({ column }) => getHeader(column, 'Blue Cards') }]
 
 const { data: teamLineup, status: teamLineupState } = await useAsyncData(
   `team/${teamId}/lineup`,
@@ -31,6 +36,70 @@ const filteredRows = computed(() => {
     })
   }) || []
 })
+
+const sorting = ref([
+  {
+    id: 'goals',
+    desc: true,
+  },
+])
+
+function getHeader(column: Column<any>, label: string) {
+  const isSorted = column.getIsSorted()
+
+  return h(
+    UDropdownMenu,
+    {
+      'content': {
+        align: 'start',
+      },
+      'aria-label': 'Actions dropdown',
+      'items': [
+        {
+          label: 'Asc',
+          type: 'checkbox',
+          icon: 'i-lucide-arrow-up-narrow-wide',
+          checked: isSorted === 'asc',
+          onSelect: () => {
+            if (isSorted === 'asc') {
+              column.clearSorting()
+            }
+            else {
+              column.toggleSorting(false)
+            }
+          },
+        },
+        {
+          label: 'Desc',
+          icon: 'i-lucide-arrow-down-wide-narrow',
+          type: 'checkbox',
+          checked: isSorted === 'desc',
+          onSelect: () => {
+            if (isSorted === 'desc') {
+              column.clearSorting()
+            }
+            else {
+              column.toggleSorting(true)
+            }
+          },
+        },
+      ],
+    },
+    () =>
+      h(UButton, {
+        'color': 'neutral',
+        'variant': 'ghost',
+        label,
+        'icon': isSorted
+          ? isSorted === 'asc'
+            ? 'i-lucide-arrow-up-narrow-wide'
+            : 'i-lucide-arrow-down-wide-narrow'
+          : 'i-lucide-arrow-up-down',
+        'class': '-mx-2.5 data-[state=open]:bg-(--ui-bg-elevated)',
+        'aria-label': `Sort by ${isSorted === 'asc' ? 'descending' : 'ascending'}`,
+      }),
+  )
+}
 </script>
 
 <template>
@@ -39,7 +108,8 @@ const filteredRows = computed(() => {
       <UInput v-model="q" placeholder="Filter ..." />
     </div>
 
-    <UTable :data="filteredRows" :columns="columns" :loading="teamLineupState === 'pending'" class="flex-1" />
+    <UTable v-model:sorting="sorting" :data="filteredRows" :columns="columns" :loading="teamLineupState === 'pending'"
+      class="flex-1" />
   </div>
 </template>
 
