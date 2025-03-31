1dbe0024e43e40330c34bf9e8ff9d8b1a86491a4
Miguel <miguel.gebhardt@bechtle.com>
2025-03-31 23:05:28 +0200

    fix: :bug: added 500 error to team lineup

--- server/api/dhb/team/lineup.ts
+++ server/api/dhb/team/lineup.ts
@@ -1,5 +1,5 @@
-import { getTeamUrl } from '../../../../server/utils/dhbUtils'
 import type { Game, Lineup, Player } from '~~/types'
+import { getTeamUrl } from '../../../../server/utils/dhbUtils'
 
 defineRouteMeta({
   openAPI: {
@@ -12,60 +12,68 @@ defineRouteMeta({
         name: 'id',
         required: true,
         example: 'handball4all.wuerttemberg.1169541',
-        summary: 'Team id'
-      }],
-  }
+        summary: 'Team id',
+      },
+    ],
+  },
 })
 
 export default defineEventHandler(async (event) => {
-  const query = getQuery(event)
-  const games: Game[] = []
+  try {
+    const query = getQuery(event)
+    const games: Game[] = []
 
-  if (!query.id) {
+    if (!query.id) {
+      throw createError({
+        statusCode: 400,
+        statusMessage: 'No id received',
+      })
+    }
+    const teamId = query.id as string
+    const teamApi = await $fetch(`/api/dhb/team/games`, {
+      query: { id: teamId },
+    })
+
+    let teamLineup = new Array<Lineup>()
+
+    await Promise.all(teamApi.map(async (game) => {
+      const lineup = await $fetch('/api/dhb/game/lineup', {
+        query: { id: game.id },
+      })
+
+      const mergePlayerStats = (player) => {
+        const existingPlayerIndex = teamLineup.findIndex(p => p.firstname === player.firstname && p.lastname === player.lastname)
+        if (existingPlayerIndex !== -1) {
+          teamLineup[existingPlayerIndex].gamesPlayed += 1
+          teamLineup[existingPlayerIndex].goals += player.goals
+          teamLineup[existingPlayerIndex].penaltyGoals += player.penaltyGoals
+          teamLineup[existingPlayerIndex].penaltyMissed += player.penaltyMissed
+          teamLineup[existingPlayerIndex].penalties += player.penalties
+          teamLineup[existingPlayerIndex].yellowCards += player.yellowCards
+          teamLineup[existingPlayerIndex].redCards += player.redCards
+          teamLineup[existingPlayerIndex].blueCards += player.blueCards
+        }
+        else {
+          teamLineup.push({
+            ...player,
+            gamesPlayed: 1,
+          })
+        }
+      }
+
+      if (game.homeTeam.id === teamId) {
+        lineup.home.forEach(homePlayer => mergePlayerStats(homePlayer))
+      }
+      else {
+        lineup.away.forEach(awayPlayer => mergePlayerStats(awayPlayer))
+      }
+    }))
+    return teamLineup
+  }
+  catch (error) {
     throw createError({
-      statusCode: 400,
-      statusMessage: 'No id received',
+      statusCode: 500,
+      statusMessage: `Error fetching team lineup data. (${error})`,
     })
   }
-  const teamId = query.id as string
-  const teamApi = await $fetch(`/api/dhb/team/games`, {
-    query: { id: teamId },
-  })
-
-  var teamLineup = Array<Lineup>()
-
-  await Promise.all(teamApi.map(async (game) => {
-    const lineup = await $fetch('/api/dhb/game/lineup', {
-      query: { id: game.id },
-    })
-
-    const mergePlayerStats = (player) => {
-      const existingPlayerIndex = teamLineup.findIndex((p) => p.firstname === player.firstname && p.lastname === player.lastname);
-      if (existingPlayerIndex !== -1) {
-        teamLineup[existingPlayerIndex].gamesPlayed += 1
-        teamLineup[existingPlayerIndex].goals += player.goals
-        teamLineup[existingPlayerIndex].penaltyGoals += player.penaltyGoals
-        teamLineup[existingPlayerIndex].penaltyMissed += player.penaltyMissed
-        teamLineup[existingPlayerIndex].penalties += player.penalties
-        teamLineup[existingPlayerIndex].yellowCards += player.yellowCards
-        teamLineup[existingPlayerIndex].redCards += player.redCards
-        teamLineup[existingPlayerIndex].blueCards += player.blueCards
-      } else {
-        teamLineup.push({
-          ...player,
-          gamesPlayed: 1,
-        })
-      }
-    }
-
-    if (game.homeTeam.id === teamId) {
-      lineup.home.forEach(homePlayer => mergePlayerStats(homePlayer));
-    } else {
-      lineup.away.forEach(awayPlayer => mergePlayerStats(awayPlayer));
-    }
-  }))
-  return teamLineup
 })
-
-
-
