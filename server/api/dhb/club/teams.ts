fc4988a199f8ac4402f61257ec3cecbddfc6df73
Miguel <miggi92@users.noreply.github.com>
2025-11-11 18:11:37 +0100

    refactor: :recycle: refactoring

--- server/api/dhb/club/teams.ts
+++ server/api/dhb/club/teams.ts
@@ -1,4 +1,5 @@
 import { getClubUrl } from '../../../../server/utils/dhbUtils'
+import type { Team } from '../../../../types'
 
 defineRouteMeta({
     openAPI: {
@@ -32,7 +33,21 @@ export default defineCachedEventHandler(async (event) => {
     const clubId = query.id as string
     const clubTeams = await $fetch(`${getClubUrl(clubId)}/teams`)
 
-    return clubTeams.data
+    return clubTeams.data.map((team: any) => {
+        return {
+            id: team.id,
+            teamGroupId: team.teamGroupId,
+            name: team.name,
+            acronym: team.acronym,
+            logo: team.logo,
+            defaultTournament: team.defaultTournament ? {
+                id: team.defaultTournament.id,
+                name: team.defaultTournament.name,
+                acronym: team.defaultTournament.acronym,
+                logo: team.defaultTournament.logo,
+            } : undefined,
+        } as Team
+    })
 }, {
     maxAge: 60 * 60 * 24 * 7, // 1 week
     name: 'club-teams',
--- server/api/dhb/teams.ts
+++ server/api/dhb/teams.ts
@@ -20,8 +20,6 @@ defineRouteMeta({
 export default defineEventHandler(async (event) => {
   // https://www.handball.net/a/sportdata/1/clubs/handball4all.wuerttemberg.36/teams
 
-  const teams: Team[] = []
-
   const query = getQuery(event)
 
   if (!query.id) {
@@ -33,20 +31,19 @@ export default defineEventHandler(async (event) => {
   const clubId = query.id as string
   const teamsApi = await $fetch(`${getClubUrl(clubId)}/teams`)
 
-  teamsApi.data.forEach((element) => {
-    teams.push({
-      id: element.id,
-      teamGroupId: element.teamGroupId,
-      name: element.name,
-      acronym: element.acronym,
-      defaultTournament: {
-        id: element.defaultTournament.id,
-        name: element.defaultTournament.name,
-        acronym: element.defaultTournament.acronym,
-        logo: element.defaultTournament.logo,
-      },
-    })
+  return teamsApi.data.map((team: any) => {
+    return {
+      id: team.id,
+      teamGroupId: team.teamGroupId,
+      name: team.name,
+      acronym: team.acronym,
+      logo: team.logo,
+      defaultTournament: team.defaultTournament ? {
+        id: team.defaultTournament.id,
+        name: team.defaultTournament.name,
+        acronym: team.defaultTournament.acronym,
+        logo: team.defaultTournament.logo,
+      } : undefined,
+    } as Team
   })
-
-  return teams
 })
