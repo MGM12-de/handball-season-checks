b495291ff2797ae278b0f5b767d517a27556c802
Miguel <miggi92@users.noreply.github.com>
2026-02-06 15:37:43 +0100

    test: :test_tube: added first passing test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,29 @@
 import { i18n, pwa } from './config/config'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const isTest = process.env.VITEST === 'true' || process.env.NODE_ENV === 'test'
+
+// Always include test-utils; exclude PWA during tests to avoid virtual module issues
+const modulesList = [
+  '@nuxt/test-utils/module',
+  '@nuxt/content',
+  '@nuxt/ui',
+  '@nuxtjs/i18n',
+  '@nuxt/image',
+  '@scalar/nuxt',
+  '@nuxt/eslint',
+  '@nuxthub/core',
+]
+
+if (!isTest) {
+  modulesList.push('@vite-pwa/nuxt')
+}
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   extends: [],
-  modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/i18n', '@nuxt/image', '@scalar/nuxt', '@nuxt/eslint', '@nuxthub/core', '@vite-pwa/nuxt'],
+  modules: modulesList,
   css: ['~/assets/css/main.css'],
   future: {
     compatibilityVersion: 4,
--- tests/components/Lineup.spec.ts
+++ tests/components/Lineup.spec.ts
@@ -6,7 +6,7 @@ describe('lineup Component', () => {
     it('renders correctly', async () => {
         const component = await mountSuspended(Lineup, {
             props: {
-                teamId: '12345',
+                teamId: 'sr.competitor.3980-132716',
             },
         })
         expect(component.text()).toBeDefined()
