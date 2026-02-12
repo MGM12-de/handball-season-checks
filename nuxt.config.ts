eb57ea28aaa3a8181d6dbc7037a63eb1cbaa6a01
Miguel <miggi92@users.noreply.github.com>
2026-02-12 18:21:45 +0100

    chore: :arrow_up: upgrade deps
    switcht to sqlite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
       namespaceId: '622b3d207e21450db456b09ce3cf34c0',
     },
     db: {
-      dialect: 'postgresql',
+      dialect: 'sqlite',
       driver: 'd1',
       connection: {
         databaseId: '29f5103a-ddac-4821-b783-060504afe479',
