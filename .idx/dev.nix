bd44ad011dd7f6182ca996517d945a35889adb72
miguel.gebhardt <miguel.gebhardt@googlemail.com>
2026-02-13 14:32:41 +0000

    chore: :arrow_up: update deps

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,15 +1,14 @@
 # To learn more about how to use Nix to configure your environment
 # see: https://developers.google.com/idx/guides/customize-idx-env
-{pkgs}: {
+{ pkgs }: {
   # Which nixpkgs channel to use.
-  channel = "stable-23.05"; # or "unstable"
+  channel = "stable-stable-25.05"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
-    pkgs.nodePackages.pnpm
+    pkgs.corepack_latest
   ];
   # Sets environment variables in the workspace
-  env = {};
+  env = { };
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -36,7 +35,7 @@
       enable = true;
       previews = {
         web = {
-          command = ["npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0"];
+          command = [ "npm" "run" "dev" "--" "--port" "$PORT" "--hostname" "0.0.0.0" ];
           manager = "web";
         };
       };
