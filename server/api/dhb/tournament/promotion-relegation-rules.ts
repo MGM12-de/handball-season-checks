5fb686e3f8b1acbc07e15c07cb51e8890672ca07
Miguel <miggi92@users.noreply.github.com>
2026-03-18 17:36:17 +0100

    chore: changed rules

--- server/api/dhb/tournament/promotion-relegation-rules.ts
+++ server/api/dhb/tournament/promotion-relegation-rules.ts
@@ -10,13 +10,13 @@ export default defineEventHandler(async (event) => {
   const rules = [
     { pattern: /sportradar\.dhbdata\.16059/, promoted: 2, relegated: 3 },
     { pattern: /m-rl-bw_bwhv/, promoted: 2, relegated: 2 },
-    { pattern: /m-ol-\d-bw_bwhv/, promoted: 1, relegated: 2 },
-    { pattern: /m-vl-\d-bw_bwhv/, promoted: 1, relegated: 3 },
-    { pattern: /m-ll-\d-bw_bwhv/, promoted: 1, relegated: 2 },
-    { pattern: /m-bol-\d-nf_nf/, promoted: 1, relegated: 4 },
+    { pattern: /m-ol-\d-bw_bwhv/, promoted: 2, relegated: 6 },
+    { pattern: /m-vl-\d-bw_bwhv/, promoted: 6, relegated: 12 },
+    { pattern: /m-ll-\d-bw_bwhv/, promoted: 12, relegated: 16 },
+    { pattern: /m-bol-\d-nf_nf/, promoted: 2, relegated: 8 },
     { pattern: /m-bl-nf_nf/, promoted: 2, relegated: 2 },
-    { pattern: /m-bk-\d-nf_nf/, promoted: 2, relegated: 2 },
-    { pattern: /m-2bk-\d-nf_nf/, promoted: 2, relegated: 0 },
+    { pattern: /m-bk-\d-nf_nf/, promoted: 4, relegated: 2 },
+    { pattern: /m-2bk-\d-nf_nf/, promoted: 4, relegated: 0 },
   ]
 
   const match = rules.find(rule => rule.pattern.test(query.id))
