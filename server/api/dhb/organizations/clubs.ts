19b9adebffc602040da6631cc670ed649720047d
Miguel <miggi92@users.noreply.github.com>
2026-03-18 17:12:00 +0100

    chore: update club data

--- server/api/dhb/organizations/clubs.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod'
-
-const querySchema = z.object({
-  id: z.string().min(1, 'Organization ID is required'),
-})
-
-export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
-})
